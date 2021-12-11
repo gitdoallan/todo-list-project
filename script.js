5bb346ddf76a4c03354535e08181170d0e815eac
gitdoallan <mailtoallan@icloud.com>
2021-12-11 14:12:28 -0300

    requisito 14

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let createBtn = document.getElementById('criar-tarefa')
-let selectOl = document.getElementById('lista-tarefas')
+const createBtn = document.getElementById('criar-tarefa')
+const selectOl = document.getElementById('lista-tarefas')
 function createLi() {
     let getInput = document.getElementById('texto-tarefa')
     newLi = document.createElement('li')
@@ -9,7 +9,7 @@ function createLi() {
 }
 createBtn.addEventListener('click', createLi)
 
-let selectInput = document.getElementById('texto-tarefa')
+const selectInput = document.getElementById('texto-tarefa')
 function submitEnter(e) {
     if (e.key === 'Enter') {
         createLi()
@@ -17,7 +17,7 @@ function submitEnter(e) {
 }
 selectInput.addEventListener('keyup', submitEnter)
 
-let olChild = selectOl.children
+const olChild = selectOl.children
 function changeBg(e) {
     for (let i=0;i<olChild.length;i+=1) {
         olChild[i].classList.remove('selected')
@@ -35,13 +35,13 @@ function completedLi(e) {
 }
 selectOl.addEventListener('dblclick', completedLi)
 
-let eraseBtn = document.getElementById('apaga-tudo')
+const eraseBtn = document.getElementById('apaga-tudo')
 function erase() {
     selectOl.innerHTML = ''
 }
 eraseBtn.addEventListener('click', erase)
 
-let removeBtn = document.getElementById('remover-finalizados')
+const removeBtn = document.getElementById('remover-finalizados')
 function removeCompleted() {
     for (let i=0;i<olChild.length;i+=1) {
         if (olChild[i].classList.contains('completed')) {
@@ -61,17 +61,3 @@ function removeSelected() {
     }
 }
 removeSelectedBtn.addEventListener('click', removeSelected)
-
-const moveUpBtn = document.getElementById('mover-cima')
-function moveUp() {
-    for (let i=0;i<olChild.length;i+=1) {
-        if (olChild[i].classList.contains('selected') && olChild[i] !== olChild[0]) {
-            saveUpInfo = olChild[i-1].innerText
-            olChild[i-1].innerText = olChild[i].innerText
-            olChild[i-1].classList.add('selected')
-            olChild[i].innerText = saveUpInfo
-            olChild[i].classList.remove('selected')
-        }
-    }
-}
-moveUpBtn.addEventListener('click', moveUp)
